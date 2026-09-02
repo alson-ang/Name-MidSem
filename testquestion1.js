5f259b331c0d26410d9566c587c12f61fe2688c3
alson-ang <alson.ang@computhink.com.sg>
2026-09-02 08:38:59 +0000

    Sep 2, 2026, 8:38 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,18 @@
 // Test
+let circle;
+
+function setup() {
+    new Canvas(600, 400);
+//    world.gravity.y = 10;
+
+function draw() {
+    background(0);
+    if (mouse.presses()) {
+        circle = new Sprite(mouse.x, mouse.y, 50);
+//        circle.collider = 'dynamic';
+}
+    }
+//    fill(random(255), random(255), random(255));
+}
+
+// circle.gravity.y
