aed438b233bd38d9118a24c123e2e724b174e299
alson-ang <alson.ang@computhink.com.sg>
2026-09-03 11:45:32 +0000

    Sep 3, 2026, 11:45 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,9 +6,7 @@ function setup() {
     new Canvas(600, 400); createCanvas(600, 400);
 
     background(0);
-
     world.gravity.y = 10;
-
     camera.x = 0; // Camera position
 
     ball = new Sprite(0, 0, 10, 10); // (x, y, width, height) or (x, y, diameter)
