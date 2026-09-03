1d0f25b976bba23aaa82a7de198745e76571af6e
alson-ang <alson.ang@computhink.com.sg>
2026-09-03 11:38:24 +0000

    Sep 3, 2026, 11:38 AM

--- testquestion1.js
+++ testquestion1.js
@@ -8,7 +8,7 @@ function setup() {
 
     world.gravity.y = 10;
 
-    ball = new Sprite(0, 0, 10, 10); // (x, y, width, height)
+    ball = new Sprite(0, 0, 10, 10); // (x, y, width, height) or (x, y, diameter)
     ball.mass = 10;
     ball.drag = 10;
     ball.bounciness = 1;
