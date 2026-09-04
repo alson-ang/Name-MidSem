82ff11bf2b9de3469386317992121a8998d77d2b
alson-ang <alson.ang@computhink.com.sg>
2026-09-04 09:48:28 +0000

    Sep 4, 2026, 9:48 AM

--- testquestion1.js
+++ testquestion1.js
@@ -9,7 +9,7 @@ function setup() {
     world.gravity.y = 10;
     camera.x = 0; // Camera position
 
-    ball = new Sprite(); // (x, y, width, height) or (x, y, diameter)
+    ball = new Sprite(0, 0, 10, 10); // (x, y, width, height) or (x, y, diameter)
     ball.x = 0;
     ball.y = 0;
     ball.diameter = 10;
