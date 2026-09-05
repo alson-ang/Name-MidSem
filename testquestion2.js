876cae83afd726e71fe5d22f47d9d4126409f5de
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 07:24:08 +0000

    Sep 5, 2026, 7:24 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,9 +1,11 @@
+
 let player;
 let floor;
+let playerAir=false;
 
 function setup(){
     new Canvas(600,400);
-    background("blue");
+    
     world.gravity.y=10;
     player=new Sprite(300,100,30,30,"dynamic");
     player.color="red";
@@ -11,7 +13,20 @@ function setup(){
     floor=new Sprite(300,350,2400,20,"static");
     floor.color="green";
 
-}
+};
 
 function draw(){
-}
+    background("blue");
+    camera.x=player.x;
+    player.vel.x=6;
+
+    if((kb.presses("space")||mouse.presses("left"))&& playerAir==false){
+        player.vel.y=-5;
+        playerAir=true;
+    };
+
+
+    if (player.collides(floor)){
+        playerAir=false;
+    };
+};
