fc4854c1bf5024fda65047e8837fd9f72a698e3a
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 06:26:14 +0000

    Sep 5, 2026, 6:26 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,13 +2,6 @@ function setup() {
     new Canvas(800, 600);
     background(150); // grayscale 0 - 255 black - white
 
-    noStroke();
-    circle(100, 100, 50);
-    
-    fill(255, 0, 0);
-    stroke(255, 255 ,0); // (r, g, b)
-    strokeWeight(10);
-    rect(200, 200, 50, 50);
 
     for (let i = 0; i < 10; i++) {
         rect(200, 200, 50, 50);
