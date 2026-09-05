118a357d4e47541512822186d5a28f8f2e32bef1
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 06:37:39 +0000

    Sep 5, 2026, 6:37 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,11 +2,9 @@ function setup() {
     new Canvas(800, 600);
     background(150); // grayscale 0 - 255 black - white
 
-    for (let i = 0; i < 10; i++) {
-        
+    for (let i = 0; i < 10; i++) {     
         fill(i * 30, i * 30, 0); // (r, g, b)
         rect(200, 200 + i * 50, 20, 20); // gradient using i
-
     }
 }
 
