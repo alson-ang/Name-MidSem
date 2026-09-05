b07b4bc9814b9a395369c7cffce068ab712a834f
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 04:27:37 +0000

    Sep 5, 2026, 4:27 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,20 +1,15 @@
-let circle;
-let ground;
-
-function setup() {
-    new Canvas(600, 400);
+function setup(){
+    new Canvas(600,400);
     world.gravity.y = 10;
-    ground = new Sprite(300, 400, 600, 30);
-    ground.collider = 'static';
+    let base = new Sprite(300, 380, 600, 30);
+    base.collider = 'static';
 }
-
-function draw() {
-    background(0);
-    if (mouse.presses()) {
-        circle = new Sprite(mouse.x, mouse.y, 50);
-        circle.collider = 'dynamic';
-        circle.bounciness = 0.2;
+function draw(){
+background(0);
+    if (mouse.presses("left")){
+        fill(random(0,225));
+        let ball = new Sprite(mouse.x,mouse.y,40);
+        ball.bounciness = 0.5;
+        ball.drag = 0.4;
     }
-
 }
-    fill(random(255), random(255), random(255));
