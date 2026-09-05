8d66113bb7832d6e319d444d5661d30312ede4b0
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 07:17:50 +0000

    Sep 5, 2026, 7:17 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,50 +1,23 @@
-// VARIABLES
-let sprite;
+let width=600;
+let height=400;
+let player;
 let floor;
 
-const jumps = 1;
-let jumpChance = jumps;
 
-function setup() {
-    new Canvas(600, 400);
-    displayMode('centered');
-    background(250);
-    world.gravity.y = 10;
 
-    sprite = new Sprite(300, 200, 30, 30, "dynamic");
-
-    floor = new Sprite(300, 400, 2400, 20, "static");
+function setup(){
+    new Canvas(600,400);
+    player=new Sprite();
+    player.width=30;
+    player.height=30;
+    floor=new Sprite();
+    floor.width=2400;
+    floor.height=20;
+    floor.y=400;
+    floor.collider="static";
+    world.gravity.y=10;
 }
 
-function preload() {
-
-}
-
-function draw() {
-    background(250);
-
-    if (sprite.x >= width / 2) {
-        camera.x = sprite.x;
-    } else {
-        camera.x = width / 2;
-    }
-
-    if (sprite.collide(floor) && jumpChance < jumps) {
-        jumpChance = jumps;
-    }
-
-    // if (sprite.y >= height / 2) {
-    //     camera.y = sprite.y;
-    // } else {
-    //     camera.y = height / 2;
-    // }
-
-    if (kb.presses("space") || mouse.presses("left")) {
-        if (jumpChance === jumps) {
-            sprite.vel.y += -5;
-            jumpChance -= 1;
-        } 
-    }
-
-    sprite.vel.x = 6
+function draw(){
+    background(255);
 }
