f1a93e72b814c17f3c2ef09ab7dfd7f19dbe3f61
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 04:33:56 +0000

    Sep 5, 2026, 4:33 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,13 +1,13 @@
 let square;
 let floor;
+let jumpChance = 0;
 
 function setup() {
     new Canvas(600,400);
-
+    
     //game
     background(0);
     world.gravity.y = 10;
-
     //square
     square = new Sprite(300,0,30,30);
     square.collider = "dynamic";
@@ -15,8 +15,32 @@ function setup() {
     //floor 
     floor = new Sprite(0,385,2400,30);
     floor.collider = "static";
+
+    //camera pos
+    camera.x = square.x;
 }
 
 function draw() {
-    background(0)
+    background(0);
+    // square.vel.x = 3;
+    camera.x = square.x;
+
+    if (jumpChance = 1) {
+        if (kb.presses("space")) {
+            //HOW TO DO 5 PIXELS?????
+            //i tryed my best to get 5 pixels
+            square.vel.y += 7;
+            console.log(jumpChance);
+        }
+        else if(mouse.presses("left")) {
+            // 5+++++++ pixel jump
+            square.vel.y += 30;
+        }
+    }
+    if (square.collides(floor)) {
+        jumpChance += 1;
+        if (jumpChance >= 2) {
+            jumpChance = 0;
+        }
+    }
 }
