f5a92bce2a337c6701215ea980a33750af21ffbf
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 04:08:32 +0000

    Sep 5, 2026, 4:08 AM

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,13 @@
-// Test
+function setup() {
+    createCanvas(600, 400);
+    background("black")
+    strokeWeight(5)
+}
+
+function draw() {
+    fill("black")
+    stroke("red")
+    circle(100, 200, 50)
+    triangle(200, 220, 280, 220, 240, 180)
+    rect(340, 180, 50, 50)
+}
