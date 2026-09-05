6d4c18a8d1dcb5b3bf3e17dc8e9b0b76e276878e
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 06:18:12 +0000

    Sep 5, 2026, 6:18 AM

--- testquestion2.js
+++ testquestion2.js
@@ -4,7 +4,7 @@ function setup() {
     new Canvas(800, 600);
     background(150); // grayscale 0 - 255 black - white
 
-    circle = circle(100, 100, 50);
+    circle(100, 100, 50);
 }
 
 function draw() {
