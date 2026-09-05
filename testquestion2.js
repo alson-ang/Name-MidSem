12a453dec2a5e4370851060414aa66c550ef7ac3
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 04:25:11 +0000

    Sep 5, 2026, 4:25 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,12 +2,13 @@ function setup() {
     createCanvas(600,400);
     background(255);
     world.gravity.y = 5;
+    floor = new Sprite(300,370,600,60,"static");
 }        
 
 function draw() {
     background(255);
     if (mouse.presses()) {
         ball = new Sprite(mouseX,mouseY,20);
-    
+    ball.bounciness = 0.9;
 }
 }
