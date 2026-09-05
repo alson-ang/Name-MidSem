bede40c8ad026db21450123b02fb7ca2aa346c47
alson-ang <alson.ang@computhink.com.sg>
2026-09-05 07:12:52 +0000

    Sep 5, 2026, 7:12 AM

--- testquestion2.js
+++ testquestion2.js
@@ -2,13 +2,16 @@
 let sprite;
 let floor;
 
+const jumps = 1;
+let jumpChance = jumps;
+
 function setup() {
     new Canvas(600, 400);
     displayMode('centered');
     background(250);
     world.gravity.y = 10;
 
-    sprite = new Sprite(300, 200, 30, 30);
+    sprite = new Sprite(300, 200, 30, 30, "dynamic");
 
     floor = new Sprite(300, 400, 2400, 20, "static");
 }
@@ -19,4 +22,29 @@ function preload() {
 
 function draw() {
     background(250);
+
+    if (sprite.x >= width / 2) {
+        camera.x = sprite.x;
+    } else {
+        camera.x = width / 2;
+    }
+
+    if (sprite.collide(floor) && jumpChance < jumps) {
+        jumpChance = jumps;
+    }
+
+    // if (sprite.y >= height / 2) {
+    //     camera.y = sprite.y;
+    // } else {
+    //     camera.y = height / 2;
+    // }
+
+    if (kb.presses("space") || mouse.presses("left")) {
+        if (jumpChance === jumps) {
+            sprite.vel.y += -5;
+            jumpChance -= 1;
+        } 
+    }
+
+    sprite.vel.x = 6
 }
